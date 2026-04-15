857c830eb9f0395777fac26cb007fcb0dfa63573
Your Name <your-email@example.com>
2026-04-15 05:36:18 +0100

    Fix Tawk.to URL - remove /default suffix

--- src/components/ui/TawkToChat.tsx
+++ src/components/ui/TawkToChat.tsx
@@ -18,7 +18,7 @@ export function TawkToChat() {
 
     const script = document.createElement('script');
     script.async = true;
-    script.src = `https://embed.tawk.to/${tawkId}/default`;
+    script.src = `https://embed.tawk.to/${tawkId}`;
     script.charset = 'UTF-8';
     script.setAttribute('crossorigin', '*');
     script.onload = () => {
